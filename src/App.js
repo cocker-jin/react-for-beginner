224d4d2c2600101271833044cc4771d05190fa6c
yeojin son <duwls0153@gmail.com>
2022-07-11 10:12:28 +0900

    firtst works

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [counter,setValue] = useState(0);
+  const [keyword,setKeyword] = useState("")
+  const onClick = () => {
+    setValue((current) => current + 1)
+  }
+  const onChange = (e) => {
+    setKeyword(e.target.value)
+  }
+  useEffect(() => {
+    console.log("i run only once.")
+  }, [])
+  useEffect(() => {
+    console.log("i run when 'keyword' changes.")
+  }, [keyword])
+  useEffect(() => {
+    console.log("i run when 'counter' changes.")
+  }, [counter])
   return (
-    <div className="App">
-      <p>시작</p>
+    <div>
+      <input 
+        onChange={onChange} 
+        value={keyword} 
+        type="text" 
+        placeholder="Search here..." 
+      />
+      <h1>{counter}</h1>
+      <button 
+        onClick={onClick}
+      > Click me</button>
     </div>
   );
 }
